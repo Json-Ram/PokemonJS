ad2008ca2c199c235c6b412223fb6f6a512a8b61
Json-Ram <jasonram778@gmail.com>
2024-03-13 01:36:23 -0700

    Add P5 preload, setup logic

---
+++ main.js
@@ -0,0 +1,28 @@
+new p5((p) => {
+  
+  let font;
+  
+  p.preload = () => {
+    font = p.loadFont("./assets/power-clear.ttf");
+  };
+
+  p.setup = () => {
+    const canvasEl = p.createCanvas(512, 384, document.getElementById("game"));
+    p.pixelDensity(3);
+    canvasEl.canvas.style = "";
+
+    p.textFont(font);
+  };
+
+  p.draw = () => {
+
+  };
+
+  p.keyPressed = (keyEvent) => {
+
+  };
+
+  p.keyReleased = () => {
+
+  };
+});
