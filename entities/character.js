6447b5952bc0519dd47cf681a3bf9d2a24743ac8
Json-Ram <jasonram778@gmail.com>
2024-03-17 00:58:54 -0700

    Add setAnim, setDirection logic to character entity

--- entities/character.js
+++ entities/character.js
@@ -11,5 +11,31 @@ export function makeCharacter(p) {
     tileHeight: 48,
     width: 32,
     height: 32,
+
+    setAnim(name) {
+      if (this.currentAnim !== name) {
+        this.currentAnim = name;
+        this.currentFrame = 0;
+        this.animationTrainer = 0;
+        this.previousTime = 0;
+      }
+    },
+
+    setDirection(direction) {
+      if (this.direction !== direction) {
+        this.direction = direction;
+      } 
+    },
+
+    setAnimFrame(animData) {
+      if (typeof animData === "number") {
+        this.currentFrame = animData;
+        return this.frames[this.currentFrame];
+      }
+
+      if (this.currentFrame === 0) {
+        this.currentFrame = animData.from;
+      }
+    }
   };
 }
