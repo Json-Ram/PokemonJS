872c26036d1840b2477b40224de444dc9bd0fe1c
Json-Ram <jasonram778@gmail.com>
2024-03-18 23:40:28 -0700

    Add camera and player entity to world scene

--- scenes/world.js
+++ scenes/world.js
@@ -1,5 +1,27 @@
+import { makeCamera } from "../entities/camera.js";
+import { makePlayer } from "../entities/player.js";
+
 export function makeWorld(p, setScene) {
   return {
-    
-  }
+    camera: makeCamera(p, 100, 0),
+    player: makePlayer(p, 0, 0),
+
+    load() {
+      this.player.load();
+    },
+
+    setup() {
+      this.player.setup();
+      this.camera.attachTo(this.player);
+    },
+
+    update() {
+      this.player.update();
+      this.camera.update();
+    },
+
+    draw() {
+      this.player.draw();
+    }
+  };
 }
