6e24310d842c19b0da5c76335ee4905baa4c2a69
Json-Ram <jasonram778@gmail.com>
2024-03-21 22:12:55 -0700

    Create map entity, Add skeleton

---
+++ entities/map.js
@@ -0,0 +1,24 @@
+export function makeTiledMap(p, x, y) {
+  return {
+    tileWidth: 32,
+    tileHeight: 32,
+    x,
+    y,
+
+    async load() {
+
+    },
+
+    prepareTiles() {
+
+    },
+
+    getSpawnPoints() {
+
+    },
+
+    draw(camera, player) {
+      
+    }
+  };
+}
