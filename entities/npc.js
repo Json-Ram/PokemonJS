2437d188b0e43aa56baa59c9a4762cb9e706cfe7
Json-Ram <jasonram778@gmail.com>
2024-03-26 22:43:07 -0700

    Add prepareAnims to npc entity

--- entities/npc.js
+++ entities/npc.js
@@ -1,3 +1,4 @@
+import { getFramesPos } from "../utils.js";
 import { makeCharacter } from "./character.js";
 
 export function makeNPC(p, x, y) {
@@ -13,5 +14,13 @@ export function makeNPC(p, x, y) {
     load() {
       this.spriteRef = p.loadImage("assets/char_f.png");
     },
+
+    prepareAnims() {
+      this.frames = getFramesPos(4, 4, this.tileWidth, this.tileHeight);
+
+      this.anims = {
+        "idle-down": 0,
+      };
+    }
   };
 }
