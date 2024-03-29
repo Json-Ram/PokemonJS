b9bb2e00e9ad626ff7b30bc619486068be229455
Json-Ram <jasonram778@gmail.com>
2024-03-28 23:43:14 -0700

    Add preventOverlap to utils

--- utils.js
+++ utils.js
@@ -65,7 +65,36 @@ export function checkCollision(objA, objB) {
     objA.x > objB.x + objB.width ||
 
     objA.y + objA.height < objB.y ||
-    
+
     objA.y > objB.y + objB.height
   );
+}
+
+export function preventOverlap(objA, objB) {
+  const overlapX =
+    Math.min(objA.x + objA.width, objB.x + objB.width) -
+    Math.max(objA.x, objB.x);
+
+  const overlapY =
+    Math.min(objA.y + objA.height, objB.y + objB.height) -
+    Math.max(objA.y, objB.y);
+
+  if (overlapX < overlapY) {
+    if (objA.x < objB.x) {
+    // right
+      objB.x = objA.x + objA.width;
+      return;
+    }
+    // left
+    objB.x = objA.x - objA.width;
+    return;
+  }
+
+  if (objA.y < objB.y) {
+    // bottom
+      objB.y = objA.y + objA.height;
+      return;
+  }
+    // top
+    objB.y = objA.y - objB.height;
 }
