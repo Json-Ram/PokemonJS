1d5a212cd248e8d6a51fef79b813dd924a477ba3
Json-Ram <jasonram778@gmail.com>
2024-03-28 23:54:26 -0700

    Add preventPassthroughFrom and preventOverlap to collidable entity

--- entities/collidable.js
+++ entities/collidable.js
@@ -1,3 +1,4 @@
+import { checkCollision, preventOverlap } from "../utils.js";
 import { debugMode } from "./debugMode.js";
 
 export function makeCollidable(p, x, y, width, height) {
@@ -10,40 +11,10 @@ export function makeCollidable(p, x, y, width, height) {
     height,
 
     preventPassthroughFrom(entity) {
-      const collision = !(
-        this.x + this.width < entity.x ||
-        this.x > entity.x + entity.width ||
-        this.y + this.height < entity.y ||
-        this.y > entity.y + entity.height
-      );
+      const collision = checkCollision(this, entity);
 
       if (collision) {
-        const overlapX = 
-          Math.min(this.x + this.width, entity.x + entity.width) -
-          Math.max(this.x, entity.x);
-        
-        const overlapY = 
-          Math.min(this.y + this.height, entity.y + entity.height) -
-          Math.max(this.y, entity.y);
-
-        if (overlapX < overlapY) {
-          if (this.x < entity.x) {
-            //right
-            entity.x = this.x + this.width;
-            return;
-          }
-          //left
-          entity.x = this.x - this.width;
-          return;
-        }
-        
-        if (this.y < entity.y) {
-          //bottom
-          entity.y = this.y + this.height;
-          return;
-        }
-          //top
-          entity.y = this.y - entity.height;
+        preventOverlap(this, entity);
       }
     },
 
