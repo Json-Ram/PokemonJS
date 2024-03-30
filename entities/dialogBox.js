f63a7af0e6a7bdf90e721c0f09c5240c3b82f3f5
Json-Ram <jasonram778@gmail.com>
2024-03-29 20:28:27 -0700

    Create dialogBox entity, Addskeleton, Add load method

---
+++ entities/dialogBox.js
@@ -0,0 +1,18 @@
+export function makeDialogBox(p, x, y) {
+  return {
+    x,
+    y,
+    spriteRef: null,
+    currentTime: 0,
+    previousTime: 0,
+    lineChars: null,
+    line: "",
+    isVisible: false,
+    onCompleteCallback: null,
+    isComplete: false,
+
+    load() {
+      this.spriteRef = p.loadImage("assets/overlay_message.png");
+    },
+  };
+}
