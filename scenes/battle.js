010c26203dabb6e9a409269ae74133af60fd15a4
Json-Ram <jasonram778@gmail.com>
2024-03-31 23:48:54 -0700

    Create battle scene, Add battle states

---
+++ scenes/battle.js
@@ -0,0 +1,11 @@
+const states = {
+  default: "default",
+  npcIntro: "npc-intro",
+  npcPokemonIntro: "npc-pokemon-intro",
+  playerPokemonIntro: "player-pokemon-intro",
+  playerTurn: "player-turn",
+  playerAttack: "player-attack",
+  npcTurn: "npc-turn",
+  battleEnd: "battle-end",
+  winnerDeclared: "winner-declared"
+}
